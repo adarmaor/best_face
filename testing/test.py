defa8cd17e47c2849aabec0030dd4a36e00f1f60
Adar Maor <you@example.com>
2019-12-18 15:58:21 +0200

    Added logger

--- testing/test.py
+++ testing/test.py
@@ -1,7 +1,11 @@
 import subprocess
 import json
 import server
+import logging
 from threading import Thread
+
+
+log = logging.getLogger(__name__)
 
 
 def test_best_image():
@@ -11,6 +15,7 @@ def test_best_image():
         '--data \'{"images":["testing/detection-1-thumbnail.jpg", "testing/detection-2-thumbnail.jpg", '
         '"testing/detection-3-thumbnail.jpg", "testing/detection-4-thumbnail.jpg", '
         '"testing/detection-5-thumbnail.jpg"]}\' http://localhost:5000/best_image')
+    log.debug(curl_response.splitlines()[-1])
     assert "testing/detection-3-thumbnail.jpg" in json.loads(curl_response.splitlines()[-1]).keys()
 
 test_best_image()
