c8974ecd79578a9502bfefc1f2e4e4a8b3d3c772
Adar Maor <you@example.com>
2019-12-18 14:20:33 +0200

    First Draft: added best image api server

---
+++ configs.py
@@ -0,0 +1,2 @@
+SUBSCRIPTION_KEY = 'c6c6229ed9a9426191671217b659b526'
+FACE_API_URL = 'https://westcentralus.api.cognitive.microsoft.com/'
---
+++ server.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+from azure.cognitiveservices.vision.face import FaceClient, models
+from msrest.authentication import CognitiveServicesCredentials
+from configs import FACE_API_URL, SUBSCRIPTION_KEY
+import os
+
+app = Flask(__name__)
+FACE_CLIENT = FaceClient(endpoint=FACE_API_URL, credentials=CognitiveServicesCredentials(SUBSCRIPTION_KEY))
+
+
+@app.route('/best_image', methods=['POST'])
+def get_best_image():
+    """
+    Recieves POST requests and returns the best image out of a list
+    MUST give full file_paths
+    :return dict: {best_image_name: azure metadata}
+    """
+    images_dict = {}
+    for image_path in request.json.get('images', ''):
+        try:
+            detection = FACE_CLIENT.face.detect_with_stream(open(image_path, 'r+b'))
+        except models._models_py3.APIErrorException:
+            continue
+        images_dict[image_path] = get_largest_face_size(detection, os.stat(image_path).st_size)
+
+    best_image = max(images_dict.keys(), key=(lambda key: images_dict[key][0]))
+    return jsonify({best_image: images_dict[best_image][1].as_dict()}), 201
+
+
+def get_largest_face_size(faces_json, full_image_size):
+    # list of tuples containing image quality and metadata per face
+    all_faces = [(full_image_size - (face.face_rectangle.height * face.face_rectangle.width), face)
+                 for face in faces_json]
+    # returns largest face in image
+    return max(all_faces, key=lambda key: key[0]) if all_faces else (0, {})
+
+
+def main():
+    app.run(port='8000')
+
+
+if __name__ == '__main__':
+    main()
